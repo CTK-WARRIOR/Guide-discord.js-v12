de728e26aead1880c845fbbaab1fc5b9bec4c147
CTK-WARRIOR <57840201+CTK-WARRIOR@users.noreply.github.com>
2020-04-02 19:22:32 +0530

    Create server.js

---
+++ Day-2/server.js
@@ -0,0 +1,50 @@
+const { Client, Collection } = require("discord.js");
+const { config } = require("dotenv");
+const {prefix, token } = require("./config.json")
+
+const client = new Client({
+    disableEveryone: true
+})
+
+// Collections
+client.commands = new Collection();
+client.aliases = new Collection();
+
+
+// Run the command loader
+["command"].forEach(handler => {
+    require(`./handlers/${handler}`)(client);
+});
+
+client.on("ready", () => {
+    console.log(`Hi, ${client.user.username} is now online!`);
+
+    client.user.setPresence("I am Devil") 
+})
+
+client.on("message", async message => {
+   
+
+    if (message.author.bot) return;
+    if (!message.guild) return;
+    if (!message.content.startsWith(prefix)) return;
+
+    // If message.member is uncached, cache it.
+    if (!message.member) message.member = await message.guild.fetchMember(message);
+
+    const args = message.content.slice(prefix.length).trim().split(/ +/g);
+    const cmd = args.shift().toLowerCase();
+    
+    if (cmd.length === 0) return;
+    
+    // Get the command
+    let command = client.commands.get(cmd);
+    // If none is found, try to find it by alias
+    if (!command) command = client.commands.get(client.aliases.get(cmd));
+
+    // If a command is finally found, run the command
+    if (command) 
+        command.run(client, message, args);
+});
+
+client.login(token);
