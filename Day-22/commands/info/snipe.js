b32f9f8209d2677978751323bbc75b85f4d812f8
CTK-WARRIOR <57840201+CTK-WARRIOR@users.noreply.github.com>
2020-07-08 10:45:21 +0530

    Create snipe.js

---
+++ Day-22/commands/info/snipe.js
@@ -0,0 +1,28 @@
+const Discord = require("discord.js")
+const config = require("../../config.json")
+const db = require("quick.db")
+
+module.exports = {
+  name: "snipe",
+  aliases: ["ms", "messagesnipe"],
+  category: "info",
+  usage: "(prefix)snipe",
+  description: "get deleted messages",
+  run:async (client, message, args) => {
+  let prefix = await db.fetch(`prefix_${message.guild.id}`)
+   if(prefix == null) {
+    prefix =  config.DEFAULT_PREFIX
+  }
+    
+    const msg = client.snipes.get(message.channel.id)
+    if(!msg) return message.channel.send("There are no deleted messages in this channel!")
+    const embed = new Discord.MessageEmbed()
+    .setAuthor(msg.author)
+    .setDescription(msg.content)
+    if(msg.image)embed.setImage(msg.image)
+    
+    message.channel.send(embed)
+   
+    
+  }
+}
